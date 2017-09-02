8b5db7389b830e04e360115e2fa97e1f8548a2ca
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-02 18:30:53 +0900

    Non Null fields and mutations

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLSchema
 } = graphql;
 
@@ -62,6 +63,25 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)},
+        companyId: {type: GraphQLString}
+      },
+      resolve(parentValue, {firstName, age}) {
+        return axios.post('http://localhost:3000/users', {firstName, age})
+          .then(response => response.data);
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: mutation
 });
