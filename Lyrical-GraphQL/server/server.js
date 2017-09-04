71984a6aa1bb6fc0d98b29ac3548ee004e27eb65
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-04 15:06:23 +0900

    https://github.com/Automattic/mongoose/issues/5399

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -15,7 +15,11 @@ if (!MONGO_URI) {
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
