c5f7f86abff8df65eda9d6cf3ab0d152f61f654d
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-04 21:00:00 +0900

    use GraphQL

--- Lyrical-GraphQL/components/SongList.js
+++ Lyrical-GraphQL/components/SongList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    const songs = this.props.data.songs;
+
     return (
-      <div>SongList</div>
+      <div>
+        {songs && <div>TITLE: {songs[0].title}</div>}
+      </div>
     );
   }
 }
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
