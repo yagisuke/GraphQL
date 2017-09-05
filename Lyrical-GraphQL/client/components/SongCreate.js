c41231468fe87e3060379caea6dca0d9d5ae415c
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-05 23:54:02 +0900

    create Song New component

---
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Create a New</h3>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
