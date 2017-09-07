9dfe5ae60fb147fed3178b1cda94c17554dad954
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-07 23:46:43 +0900

    add new song submit func

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -7,11 +9,17 @@ class SongCreate extends Component {
     this.state = { title: '' };
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.props)
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={ event => this.setState({ title: event.target.value }) }
@@ -23,4 +31,12 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
