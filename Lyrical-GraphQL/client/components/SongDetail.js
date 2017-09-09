4e8c1cc74c15fabcb88782657f6495e78cb71cb7
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-09 18:16:21 +0900

    show song data on song detail page.

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    if (this.props.data.loading) return <div>Loading...</div>;
+
+    const {id, title} = this.props.data.song;
+
     return (
       <div>
         <h3>Song Detail</h3>
+        <p>{id}</p>
+        <p>{title}</p>
       </div>
     )
   }
 }
 
-export default SongDetail;
+export default graphql(query, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -28,9 +28,7 @@ class SongList extends Component {
   }
 
   render() {
-    if (this.props.data.loading) {
-      return <div>Loading...</div>;
-    }
+    if (this.props.data.loading) return <div>Loading...</div>;
 
     return (
       <div>
