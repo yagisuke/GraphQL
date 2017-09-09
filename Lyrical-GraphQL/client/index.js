0d521d6bf29bdb7d796afcc3881c34700e7a2bb7
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-10 00:52:09 +0900

    catching with datafromobject

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -8,7 +8,9 @@ import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
 import SongDetail from './components/SongDetail';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
