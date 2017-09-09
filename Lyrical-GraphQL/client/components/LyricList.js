72ab84e3a82925dc132001291dd9834df8d51012
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-10 00:33:10 +0900

    add lyric list on song detail page.

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  renderLyrics() {
+    return this.props.lyrics.map(({ content }, index) => {
+      return (
+        <li key={index} className="collection-item">
+          {content}
+        </li>
+      );
+    });
+  }
+
   render() {
+    console.log(this.props)
     return (
-      <ul>
-        LyricList
-      </ul>
+      <ul className="collection">{this.renderLyrics()}</ul>
     )
   }
 }
