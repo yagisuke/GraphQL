8c6aa2af021bef6a2095511ce6df2819827ed7db
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-09-10 01:12:22 +0900

    add thumb icon

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  onLike(id) {
+      console.log(id)
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ content }, index) => {
+    return this.props.lyrics.map(({ id, content }, index) => {
       return (
         <li key={index} className="collection-item">
           {content}
+          <i
+            className="material-icons"
+            onClick={() => this.onLinke(id)}
+          >
+            thumb_up
+          </i>
         </li>
       );
     });
