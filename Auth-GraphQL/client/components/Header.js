5d2894002d55c0bdf858b1c8ac6672e5aaa7fcf8
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-11-21 08:03:53 +0900

    authentication state

--- Auth-GraphQL/client/components/Header.js
+++ Auth-GraphQL/client/components/Header.js
@@ -3,11 +3,31 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (this.props.data.loading) {
+      return <div />;
+    }
+
+    if (user) {
+      return (
+        <div>Logout</div>
+      )
+    } else {
+      return (
+        <div>`You're not signed in.`</div>
+      )
+    }
+  }
+
   render() {
     return(
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
