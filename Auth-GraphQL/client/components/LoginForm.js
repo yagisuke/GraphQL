c3be2de229633640f67b5f7a843032d315d570b7
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-11-23 18:12:50 +0900

    fix the login process

--- Auth-GraphQL/client/components/LoginForm.js
+++ Auth-GraphQL/client/components/LoginForm.js
@@ -3,12 +3,19 @@ import AuthForm from './AuthForm'
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Login';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard')
+    }
   }
 
   onSubmit({ email, password }) {
@@ -34,4 +41,6 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(
+  graphql(mutation)(LoginForm)
+);
