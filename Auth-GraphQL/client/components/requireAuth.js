cc7d56d5ff134683a6fc643916760a2926602fed
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-11-24 00:37:05 +0900

    fix

--- Auth-GraphQL/client/components/requireAuth.js
+++ Auth-GraphQL/client/components/requireAuth.js
@@ -5,9 +5,9 @@ import { hashHistory } from 'react-router';
 
 export default (WrappedComponent) => {
   class RequireAuth extends Component {
-    componentDidMount() {
-      if (!this.props.data.loading && !this.props.data.user) {
-        hashHistory.push('./login');
+    componentWillUpdate(nextProps) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
+        hashHistory.push('/login');
       }
     }
 
