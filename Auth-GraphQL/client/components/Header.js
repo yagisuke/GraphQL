c8b6b0407483a11ca2bca605e15da72a4c48aa61
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-11-24 00:12:16 +0900

    add ;

--- Auth-GraphQL/client/components/Header.js
+++ Auth-GraphQL/client/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router'
 import query from '../queries/CurrentUser';
-import mutation from '../mutations/Logout'
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
   onLogoutClick() {
